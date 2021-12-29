f8641697f43845eb1408de03094d7e5474f35254
Pablo <pglez82@gmail.com>
2021-12-29 08:26:56 +0000

    dockerizing everyting and adding graphana and prometheus

--- restapi/server.ts
+++ restapi/server.ts
@@ -1,25 +1,27 @@
-import express, { Application } from "express";
+import express, { Application, RequestHandler } from "express";
 import cors from 'cors';
 import bp from 'body-parser';
+import promBundle from 'express-prom-bundle';
 import api from "./api"; 
 
 const app: Application = express();
-const port = 5000;
+const port: number = 5000;
 
 const options: cors.CorsOptions = {
   origin: ['http://localhost:3000']
 };
+
+const metricsMiddleware:RequestHandler = promBundle({includeMethod: true});
+app.use(metricsMiddleware);
 
 app.use(cors(options));
 app.use(bp.json());
 
 app.use("/api", api)
 
-try {
-    app.listen(port, (): void => {
-        console.log(`Connected successfully on port ${port}`);
-    });
-} catch (error) {
-    console.error(`Error occured: ${error.message}`);
-}
+app.listen(port, ():void => {
+    console.log('Restapi listening on '+ port);
+}).on("error",(error:Error)=>{
+    console.error('Error occured: ' + error.message);
+});
 
--- webapp/server.ts
+++ webapp/server.ts
@@ -2,6 +2,13 @@ import express,{Application} from 'express';
 //for using an import here we need to configure the tsconfig.json
 //setting the option module to commonjs
 
-var app:Application = express()
+var app: Application = express()
+const port: number = 3000;
+
 app.use(express.static('build'))
-app.listen(3000)
+
+app.listen(port, ():void => {
+    console.log('Webapp started on port '+ port);
+}).on("error",(error:Error)=>{
+    console.error('Error occured: ' + error.message);
+});
