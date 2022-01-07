88ae5b5d1595c88f0119784b6fe68d9ca28cca0a
Pablo <pglez82@gmail.com>
2022-01-07 12:21:13 +0000

    mocking tests

--- webapp/src/components/EmailForm.test.tsx
+++ webapp/src/components/EmailForm.test.tsx
@@ -1,43 +1,33 @@
-import { render,fireEvent } from "@testing-library/react";
+import { render, fireEvent, act } from "@testing-library/react";
 import EmailForm from "./EmailForm";
+import {User} from './../shared/shareddtypes';
+import * as api from './../api/api'
 
-test('check that everything is rendering propertly', async () => {
-  const { getByText } = render(<EmailForm OnUserListChange={()=>{}}/>);
-  const button = getByText("Accept");
-  expect(button).toBeInTheDocument();
-  fireEvent.click(button);
-});
-
-test('check register ok', async () => {
-  const { container, getByText} = render(<EmailForm OnUserListChange={()=>{}}/>);
-  const inputName = container.querySelector('input[name="username"]')!; //Exclamation means we ensure that the value is never null
-  const inputEmail = container.querySelector('input[name="email"]')!;
-  fireEvent.change(inputName, { target: { value: "Pablo" } });
-  fireEvent.change(inputEmail, { target: { value: "gonzalezgpablo@uniovi.es" } });
-
-  jest.mock("../api/api", () => {
-    return {
-      addUser: jest.fn(() => {return true})
-    };
-  });
-
-  const button = getByText("Accept");
-  fireEvent.click(button);
-})
+jest.mock('../api/api');
 
 test('check register fail', async () => {
-  const { container, getByText} = render(<EmailForm OnUserListChange={()=>{}}/>);
-  const inputName = container.querySelector('input[name="username"]')!;
-  const inputEmail = container.querySelector('input[name="email"]')!;
-  fireEvent.change(inputName, { target: { value: "Pablo" } });
-  fireEvent.change(inputEmail, { target: { value: "gonzalezgpablo@uniovi.es" } });
-
-  jest.mock("../api/api", () => {
-    return {
-      addUser: jest.fn(() => {return false})
-    };
+  jest.spyOn(api,'addUser').mockImplementation((user:User):Promise<boolean> => Promise.resolve(false))
+  await act(async () => {    
+    const {container, getByText} = render(<EmailForm OnUserListChange={()=>{}}/>)  
+    const inputName = container.querySelector('input[name="username"]')!;
+    const inputEmail = container.querySelector('input[name="email"]')!;
+    fireEvent.change(inputName, { target: { value: "Pablo" } });
+    fireEvent.change(inputEmail, { target: { value: "gonzalezgpablo@uniovi.es" } });
+    const button = getByText("Accept");
+    fireEvent.click(button);
   });
+})
 
-  const button = getByText("Accept");
-  fireEvent.click(button);
+test('check register ok', async () => {
+  
+  jest.spyOn(api,'addUser').mockImplementation((user:User):Promise<boolean> => Promise.resolve(true))
+  await act(async () => {    
+    const {container, getByText} = render(<EmailForm OnUserListChange={()=>{}}/>)  
+    const inputName = container.querySelector('input[name="username"]')!;
+    const inputEmail = container.querySelector('input[name="email"]')!;
+    fireEvent.change(inputName, { target: { value: "Pablo" } });
+    fireEvent.change(inputEmail, { target: { value: "gonzalezgpablo@uniovi.es" } });
+    const button = getByText("Accept");
+    fireEvent.click(button);
+  });
 })
