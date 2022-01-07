2f46782ca1cc16a79fc3a32697791ee553aab29a
Pablo <pglez82@gmail.com>
2022-01-07 13:54:02 +0000

    testing docker push

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -2,6 +2,4 @@ export default {
     transform: {
         "^.+\\.tsx?$": "ts-jest"
     },
-    collectCoverage: true,
-    collectCoverageFrom:["!src/api/api.ts","!src/reportWebVitals.ts","!src/index.tsx"]
 }
