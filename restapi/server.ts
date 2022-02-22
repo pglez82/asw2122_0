a20b66544f89214ce09f10a95470ec09db5f6bec
Pablo Gonzalez <pglez82@gmail.com>
2022-02-22 18:18:01 +0100

    configuring cors

--- restapi/server.ts
+++ restapi/server.ts
@@ -7,14 +7,10 @@ import api from "./api";
 const app: Application = express();
 const port: number = 5000;
 
-const options: cors.CorsOptions = {
-  origin: ['http://localhost:3000']
-};
-
 const metricsMiddleware:RequestHandler = promBundle({includeMethod: true});
 app.use(metricsMiddleware);
 
-app.use(cors(options));
+app.use(cors());
 app.use(bp.json());
 
 app.use("/api", api)
